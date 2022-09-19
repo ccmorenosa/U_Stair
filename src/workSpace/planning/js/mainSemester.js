a612f3b55f3740076f57eb964d91dc92bb5adda9
Catalina Sarria <afmorenosa@unal.edu.co>
2022-09-19 14:14:34 -0500

    Update Name

--- src/workSpace/planning/js/mainSemester.js
+++ src/workSpace/planning/js/mainSemester.js
@@ -1,6 +1,6 @@
 /*
 ** UStair.  Grade curriculum schedule.
-** Copyright (C) 2020  Andŕes Felipe Moreno Sarria
+** Copyright (C) 2020  Cindy Catalina Moreno Sarria
 **
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
